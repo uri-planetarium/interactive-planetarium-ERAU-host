f0659854f18f761e55fb1c9b860f0a5e9795e48f
Shinkyuuu <codyspark@gmail.com>
2022-01-17 23:24:22 +0000

    Added security for player_id's by changing the database.sql player_id generation

--- host_client/src/App.js
+++ host_client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 
 import CreateGame from "./components/createGame/create_game";
+import GameLobby from "./components/gameLobby/game_lobby";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<CreateGame />}/>
+                <Route path="/lobby" element={<GameLobby />}/>
             </Routes>
         </Router>
     );
