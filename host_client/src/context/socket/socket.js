e4877fe6e4aa7a962c9906b6b431c6fd32d2a667
shinkyuuu <codyspark@gmail.com>
2022-02-11 02:45:37 +0000

    Fixed CORS

--- host_client/src/context/socket/socket.js
+++ host_client/src/context/socket/socket.js
@@ -5,7 +5,7 @@ import socketio from "socket.io-client";
 const SOCKET_URL = (process.env.NODE_ENV === "production") ? "https://erau-interplanet-host.herokuapp.com/" : "http://localhost:5001/";
 
 export const socket = socketio.connect(SOCKET_URL, {  
-    withCredentials: true,
+    withCredentials: false,
     closeOnBeforeunload: false
 });
 export const SocketContext = React.createContext();
